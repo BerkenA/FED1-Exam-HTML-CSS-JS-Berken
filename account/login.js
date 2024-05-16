79f7628ce72cafac4d5f11f3b51cf9c51c2e5414
LAPTOP-5RU3UB56\Berke <berken_93@hotmail.com>
2024-05-16 13:44:01 +0200

    Finalized my login. It directs to the right pages now. Also added a logout timer that tracks inactivity. Logging you out after 30 minutes of inactivity. Working on sign up/ register page.

--- account/login.js
+++ account/login.js
@@ -1,14 +1,13 @@
 const emailLogIn = document.getElementById('email');
 const password = document.getElementById('password');
+const submitBtn = document.getElementById("submit");
+const registBtn = document.getElementById("registerButton")
 const getToken = window.localStorage.getItem("Bearer Token")
 const userId = window.localStorage.getItem("User Storage")
-const timeDate = new Date();
-const minutes = timeDate.getMinutes();
 
-console.log(minutes)
 
-const submitBtn = document.getElementById("submit");
-submitBtn.onclick = function fetchLogin() {
+// Function for logging in
+function fetchLogin() {
     let passwordValue = password.value;
     let emailValue = emailLogIn.value;
     const postData = {
@@ -16,7 +15,6 @@ submitBtn.onclick = function fetchLogin() {
         "password": passwordValue
     };
 
-    console.log(postData);
     fetch('https://v2.api.noroff.dev/auth/login', {
         method: 'POST',
         headers: {
@@ -33,14 +31,30 @@ submitBtn.onclick = function fetchLogin() {
     })
     .then(json => {
         const fetchResult = json.data;
-        console.log(fetchResult);
         const bearerToken = fetchResult.accessToken
         const userToken = fetchResult.name
         window.localStorage.setItem("Bearer Token", bearerToken)
         window.localStorage.setItem("User Storage", userToken)
+        window.location.href=`/post/index.html`
     })
     .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
         alert('An error occurred while logging in');
     });
 };
+
+// Event listener for pressing Enter key in password field
+password.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        fetchLogin();
+    }
+});
+
+// Event listener for clicking the submit button
+submitBtn.onclick = function() {
+    fetchLogin();
+};
+
+registBtn.onclick = function(){
+    window.location.href =`/account/register.html`
+}
